312a53d18abfaaf50073375daeb72fa9091a2a2b
SeeAllcom <36596847+SeeAllcom@users.noreply.github.com>
2019-02-19 17:30:43 +0200

    фікс

--- js_files/page_users_select.php
+++ js_files/page_users_select.php
@@ -91,87 +91,120 @@ if (isset($_SESSION['id_user'])) {
 }
 ?>
 <div class="right_block">
-	<p>оновлень: <?php echo $num_news_select; ?> | <a href="#">друзі: <?php $counter = 0;
-	foreach ($row_user_readers_subscription as $email_read) {
-		foreach ($row_user_subscription as $email_sub) {
-			if ($email_read[5] == $email_sub[5]) {
-				$counter += 1;
-			}
-		}
-	}
-	echo $counter; ?></a> | <a href="#">читачів: <?php echo $num_user_readers_subscription; ?></a> | <a href="#">відстежує: <?php echo $num_user_subscription; ?></a></p>
-	<div class="open_more_information_users" onclick="open_more_information_users()">Больше информации о пользователе</div>
-	<div class="close_more_information_users" onclick="close_more_information_users()">Скрыть информацию о пользователе</div>
-	<div  id="more_information_users">
-		<table>
-			<tr>
-				<td><?php echo $sm; ?>: <?php if (!empty($row_more_info['sharp'])) {
-					echo $row_more_info['sharp'].' см';
-				} else { echo $not_specified; }?></td>
-				<td><?php echo $kg; ?>: <?php if (!empty($row_more_info['weight'])) {
-					echo $row_more_info['weight'].' кг';
-				} else { echo $not_specified; }?></td>
-			</tr>
-			<tr>
-				<td><?php echo $color_eyes; ?>: <?php if (!empty($row_more_info['eyes'])) {
-					echo $eyes;
-				} else { echo $not_specified; } ?></td>
-				<td><?php echo $color_heir; ?>: <?php if (!empty($row_more_info['hair'])) {
-					echo $hair;
-				} else { echo $not_specified; } ?></td>
-			</tr>
-
-		</table>
-		<table>
-			<tr>
-				<td><?php echo $hobby; ?>: <?php if (!empty($row_more_info['hobby'])) {
-					echo $row_more_info['hobby'];
-				} else { echo $not_specified; } ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $purpose_life; ?>: <?php if (!empty($row_more_info['purpose_life'])) {
-					echo $row_more_info['purpose_life'];
-				} else { echo $not_specified; } ?></td>
-			</tr>
-			<tr>
-				<td><?php echo $religion; ?>: <?php if (!empty($row_more_info['religion'])) {
-					echo $row_more_info['religion'];
-				} else { echo $not_specified; } ?></td>
-			</tr>
-		</table>
-	</div>
-	<div class="choing_post_user_block">
-		<div class="choing_post"><a class="open_table" onclick="open_table()"><i class="fas fa-table"></i> Таблиця</a> | <a class="open_tape" data-iduser="<?php echo $id_user; ?>" data-emailuser="<?php echo $email_user; ?>" onclick="open_tape(this)"><i class="fas fa-tape"></i> Стрічка</a></div>
-	</div>
-	<ul class="post_user_block">
-		<?php if ($num_news_select != 0){ 
-			foreach ($row_news_select as $news_table) { 
-				$query_gallery_news_select = "SELECT * FROM `gallery_news` WHERE email_users_news='$news_table[1]' AND kod_news='$news_table[5]'";
-				$result_gallery_news_select = mysqli_query($db, $query_gallery_news_select);
-				$num_gallery_news_select = mysqli_num_rows($result_gallery_news_select);
-				$row_gallery_news_select = mysqli_fetch_array($result_gallery_news_select); ?>
-				<li onclick="open_modal()">
-					<span data-iduser="<?php echo $id_user; ?>" data-emailuser="<?php echo $news_table[1]; ?>" data-kodnews="<?php echo $news_table[5]; ?>" onclick="open_user_post(this)" class="descr"><i class="far fa-comment-alt"></i> 25 | <i class="fas fa-brain"></i> 11</span>
-					<?php if ($num_gallery_news_select != 0) { ?>
-					<img src="<?php echo '../gallery/news/'.$row_gallery_news_select[3]; ?>">
-					<?php }
-					else { ?> 
-
-					<div class="text_left"><?php echo $news_table[6]; ?></div>	
-					<?php } ?>
-				</li>
-				<?php } ?>
-				<div id="open_user_post" style="display: none;"></div>
-				<?php } ?>		
-			</ul>
-			<div id="news_user_tape"></div>
+		<div class="counts_module">
+			<a class="page_counter">
+				<div class="count">
+					<?php echo $num_news_select; ?>
+				</div>
+				<div class="label">
+					оновлень
+				</div>
+			</a>
+			<a href="#" class="page_counter">
+				<div class="count">
+					<?php $counter = 0;
+					foreach ($row_user_readers_subscription as $email_read) {
+						foreach ($row_user_subscription as $email_sub) {
+							if ($email_read[5] == $email_sub[5]) {
+								$counter += 1;
+							}
+						}
+					}
+					echo $counter; ?>
+				</div>
+				<div class="label">
+					друзі
+				</div>
+			</a>
+			<a href="#" class="page_counter">
+				<div class="count">
+					<?php echo $num_user_readers_subscription; ?>
+				</div>
+				<div class="label">
+					читачів
+				</div>
+			</a>
+			<a href="#" class="page_counter">
+				<div class="count">
+					<?php echo $num_user_subscription; ?>
+				</div>
+				<div class="label">
+					відстежує
+				</div>
+			</a>
 		</div>
+		<div class="open_more_information_users" onclick="open_more_information_users()">Больше информации о пользователе</div>
+		<div class="close_more_information_users" onclick="close_more_information_users()">Скрыть информацию о пользователе</div>
+		<div  id="more_information_users">
+			<table>
+				<tr>
+					<td><?php echo $sm; ?>: <?php if (!empty($row_more_info['sharp'])) {
+						echo $row_more_info['sharp'].' см';
+					} else { echo $not_specified; }?></td>
+					<td><?php echo $kg; ?>: <?php if (!empty($row_more_info['weight'])) {
+						echo $row_more_info['weight'].' кг';
+					} else { echo $not_specified; }?></td>
+				</tr>
+				<tr>
+					<td><?php echo $color_eyes; ?>: <?php if (!empty($row_more_info['eyes'])) {
+						echo $eyes;
+					} else { echo $not_specified; } ?></td>
+					<td><?php echo $color_heir; ?>: <?php if (!empty($row_more_info['hair'])) {
+						echo $hair;
+					} else { echo $not_specified; } ?></td>
+				</tr>
+
+			</table>
+			<table>
+				<tr>
+					<td><?php echo $hobby; ?>: <?php if (!empty($row_more_info['hobby'])) {
+						echo $row_more_info['hobby'];
+					} else { echo $not_specified; } ?></td>
+				</tr>
+				<tr>
+					<td><?php echo $purpose_life; ?>: <?php if (!empty($row_more_info['purpose_life'])) {
+						echo $row_more_info['purpose_life'];
+					} else { echo $not_specified; } ?></td>
+				</tr>
+				<tr>
+					<td><?php echo $religion; ?>: <?php if (!empty($row_more_info['religion'])) {
+						echo $row_more_info['religion'];
+					} else { echo $not_specified; } ?></td>
+				</tr>
+			</table>
+		</div>
+		<div class="choing_post_user_block">
+			<div class="choing_post"><a class="open_table" onclick="open_table()"><i class="fas fa-table"></i> Таблиця</a> | <a class="open_tape" data-iduser="<?php echo $id_user; ?>" data-emailuser="<?php echo $email_user; ?>" onclick="open_tape(this)"><i class="fas fa-tape"></i> Стрічка</a></div>
+		</div>
+		<ul class="post_user_block">
+			<?php if ($num_news_select != 0){ 
+				foreach ($row_news_select as $news_table) { 
+					$query_gallery_news_select = "SELECT * FROM `gallery_news` WHERE email_users_news='$news_table[1]' AND kod_news='$news_table[5]'";
+					$result_gallery_news_select = mysqli_query($db, $query_gallery_news_select);
+					$num_gallery_news_select = mysqli_num_rows($result_gallery_news_select);
+					$row_gallery_news_select = mysqli_fetch_array($result_gallery_news_select); ?>
+					<li onclick="open_modal()">
+						<span data-iduser="<?php echo $id_user; ?>" data-emailuser="<?php echo $news_table[1]; ?>" data-kodnews="<?php echo $news_table[5]; ?>" onclick="open_user_post(this)" class="descr"><i class="far fa-comment-alt"></i> 25 | <i class="fas fa-brain"></i> 11</span>
+						<?php if ($num_gallery_news_select != 0) { ?>
+						<img src="<?php echo '../gallery/news/'.$row_gallery_news_select[3]; ?>">
+						<?php }
+						else { ?> 
+
+						<div class="text_left"><?php echo $news_table[6]; ?></div>	
+						<?php } ?>
+					</li>
+					<?php } ?>
+					<div id="open_user_post" style="display: none;"></div>
+					<?php } ?>		
+				</ul>
+				<div id="news_user_tape"></div>
+			</div>
 
 
-					<script type="text/javascript">
-						$('#open_user_post').on('shown.bs.modal', function () {
-							$('html').css('overflow','hidden');
-						}).on('hidden.bs.modal', function() {
-							$('html').css('overflow','auto');
-						});
-					</script>
+			<script type="text/javascript">
+				$('#open_user_post').on('shown.bs.modal', function () {
+					$('html').css('overflow','hidden');
+				}).on('hidden.bs.modal', function() {
+					$('html').css('overflow','auto');
+				});
+			</script>
